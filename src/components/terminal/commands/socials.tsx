ac1cd82f3073775d876413d946697725ed1aabd5
Berikai <berkayeren2009@gmail.com>
2024-03-25 23:27:09 +0300

    Fix new tab

--- src/components/terminal/commands/socials.tsx
+++ src/components/terminal/commands/socials.tsx
@@ -4,10 +4,10 @@ export default terminal => ({
     description: "Social media links",
     hidden: true,
     execute: () => {
-        terminal.console.push(<TerminalText text={<a href="https://www.github.com/Berikai">[<span class="text-gray-300"> Github </span>]</a>} />)
-        terminal.console.push(<TerminalText text={<a href="https://soundcloud.com/verdantbass">[<span class="text-orange-300"> Soundcloud </span>]</a>} />)
-        terminal.console.push(<TerminalText text={<a href="https://discord.com/users/verdantbass">[<span class="text-blue-300"> Discord </span>]</a>} />)
-        terminal.console.push(<TerminalText text={<a href="https://www.instagram.com/verdantbass">[<span class="text-pink-300"> Instagram </span>]</a>} />)
-        terminal.console.push(<TerminalText text={<a href="https://www.twitter.com/verdantbass">[<span class="text-cyan-300"> Twitter </span>]</a>} />)
+        terminal.console.push(<TerminalText text={<a href="https://www.github.com/Berikai" target="_blank" rel="noopener noreferrer">[<span class="text-gray-300"> Github </span>]</a>} />)
+        terminal.console.push(<TerminalText text={<a href="https://soundcloud.com/verdantbass" target="_blank" rel="noopener noreferrer">[<span class="text-orange-300"> Soundcloud </span>]</a>} />)
+        terminal.console.push(<TerminalText text={<a href="https://discord.com/users/verdantbass" target="_blank" rel="noopener noreferrer">[<span class="text-blue-300"> Discord </span>]</a>} />)
+        terminal.console.push(<TerminalText text={<a href="https://www.instagram.com/verdantbass" target="_blank" rel="noopener noreferrer">[<span class="text-pink-300"> Instagram </span>]</a>} />)
+        terminal.console.push(<TerminalText text={<a href="https://www.twitter.com/verdantbass" target="_blank" rel="noopener noreferrer">[<span class="text-cyan-300"> Twitter </span>]</a>} />)
     }
 })
