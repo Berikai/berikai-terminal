ed320c8c7c9a9b42509c01bba45f3f43ab35643d
Berikai <berkayeren2009@gmail.com>
2024-06-26 16:25:10 +0300

    Add array support for welcome text

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,8 +1,19 @@
 export default function Welcome({ title, description }) {
+    const descriptionElements = [];
+
+    if (Array.isArray(description)) {
+        for (let i = 0; i < description.length; i++) {
+            const element = description[i];
+            descriptionElements.push(<p className="text-lg text-gray-600">{element}</p>);
+        }
+    } else {
+        descriptionElements.push(<p className="text-lg text-gray-600">{description}</p>);
+    }
+
     return (
-            <div class="flex flex-col items-center">
-                <h1 class="mb-2 mt-4 text-4xl font-bold text-gray-800">{title}</h1>
-                <p class="text-lg text-gray-600">{description}</p>
-            </div>
+        <div className="flex flex-col items-center">
+            <h1 className="mb-2 mt-4 text-4xl font-bold text-gray-800">{title}</h1>
+            {descriptionElements}
+        </div>
     )
 }
