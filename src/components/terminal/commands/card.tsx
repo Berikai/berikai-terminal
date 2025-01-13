24ad267995b9f2a5db38d864febfa6e516d3c706
Berikai <berkayeren2009@gmail.com>
2025-01-13 05:42:01 +0300

    Add increased width for new card

--- src/components/terminal/commands/card.tsx
+++ src/components/terminal/commands/card.tsx
@@ -9,8 +9,8 @@ export default terminal => ({
         terminal.console.push(<TerminalText text="Opening card.berikai.dev..." />)
         
         const new_id = GenerateID()
-        const _tailwind_gen = <div class="h-[52rem] w-[28rem]"></div>;_tailwind_gen
-        const new_process = <BrowserWindow height={'[52rem]'} weight={'[28rem]'} id={new_id} key={new_id}><iframe src="https://card.berikai.dev" width="100%" height="100%"></iframe></BrowserWindow>
+        const _tailwind_gen = <div class="h-[52rem] w-[36rem]"></div>;_tailwind_gen
+        const new_process = <BrowserWindow height={'[52rem]'} weight={'[36rem]'} id={new_id} key={new_id}><iframe src="https://card.berikai.dev" width="100%" height="100%"></iframe></BrowserWindow>
         terminal.newProcess(new_id, new_process)
     }
 })
