7eaa65a1f0d9ac4591ab6d25803da8bd27017f55
Berikai <berkayeren2009@gmail.com>
2025-01-15 07:03:41 +0300

    Fix card.berikai.dev domain

--- src/components/terminal/commands/card.tsx
+++ src/components/terminal/commands/card.tsx
@@ -6,11 +6,11 @@ export default terminal => ({
     description: "View my card",
     hidden: false,
     execute: () => {
-        terminal.console.push(<TerminalText text="Opening card.berikai.dev..." />)
+        terminal.console.push(<TerminalText text="Opening berikai.dev..." />)
         
         const new_id = GenerateID()
         const _tailwind_gen = <div class="h-[52rem] w-[36rem]"></div>;_tailwind_gen
-        const new_process = <BrowserWindow height={'[52rem]'} width={'[48rem]'} id={new_id} key={new_id}><iframe src="https://card.berikai.dev" width="100%" height="100%"></iframe></BrowserWindow>
+        const new_process = <BrowserWindow height={'[52rem]'} width={'[48rem]'} id={new_id} key={new_id}><iframe src="https://berikai.dev" width="100%" height="100%"></iframe></BrowserWindow>
         terminal.newProcess(new_id, new_process)
     }
 })
